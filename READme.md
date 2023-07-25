6ab3265ee76a3deb5e6a5cd2a5cd9eb0bf48dc86
noot-noot-pengu <random_dude_233@proton.me>
2023-07-25 23:30:41 +0100

    conf

--- READme.md
+++ READme.md
@@ -1,1 +1,19 @@
 neovim config from source: [nvchad](https://nvchad.com/)
+
+```bash
+    rustup component add rust-analyzer  # maybe required i don't remember
+    sudo pacman -S lldb
+```
+
+
+
+## debugging
+create breakpoints :DapToggleBreakpoint
+
+open debugger in rust with :RustDebuggable
+
+
+```lua
+lua local widgets = require("dap.ui.widgets"); local sidebar = widgets.sidebar(widgets.scopes); sidebar.open()
+```
+go line by line :DapStepOver
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -24,6 +24,33 @@ local plugins = {
     {
         "simrat39/rust-tools.nvim",
         ft = "rust",
+        dependencies = "neovim/nvim-lspconfig",
+        opts = function()
+            return require "custom.configs.rust-tools"
+        end,
+        config = function(_, opts)
+            require("rust-tools").setup(opts)
+        end
+    },
+    {
+        "mfussenegger/nvim-dap"
+    },
+    {
+        "saecki/crates.nvim",
+        ft = {"rust", "toml"},
+        config = function(_, opts)
+            local crates = require("crates")
+            crates.setup(opts)
+            crates.show()
+        end
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        opts = function()
+            local M = require "plugins.configs.cmp"
+            table.insert(M.sources, {name = "crates"})
+            return M
+        end
     }
 }
 return plugins
